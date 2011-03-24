ba7d7a501305b75f57dc6fa66479a1c0d1adbc57
Simon Tokumine <simon@japancentre.com>
2011-03-24 14:42:58 +0000

    update settings and XML stylesheet to use cartodb standard names

--- server/settings.js
+++ server/settings.js
@@ -3,11 +3,12 @@ var path = require('path');
 module.exports.styles = path.join(__dirname,'styles');
 
 module.exports.postgis = {
-    'dbname' : 'tiledb',
+    'dbname' : 'cartodb_dev_user_1_db',
+    'host'   : '127.0.0.1',
     'extent' : '-20005048.4188,-9039211.13765,19907487.2779,17096598.5401',
     'geometry_field' : 'the_geom',
-    'srid' : 900913,
-    'user' : 'postgres',
+    'srid' : 3785,
+    'user' : 'publicuser',
     'max_size' : 1,
     'type' : 'postgis'
 };
