9ca3505e3357c46176fd92e346dee1e0f472ad02
Simon Tokumine <simon@japancentre.com>
2011-04-07 23:49:59 +0100

    cleanup hydra branch

--- app.js
+++ app.js
@@ -12,9 +12,9 @@
 // sanity check arguments
 var ENV = process.argv[2]
 var APP = process.argv[3]
-if ((ENV != 'development' && ENV != 'production') || (APP != 'proxy' && APP != 'tiler' && APP != 'old_tiler' && APP != 'carto_tiler' && APP != 'cached_carto_tiler' )){
+if ((ENV != 'development' && ENV != 'production') || (APP != 'proxy' && APP != 'tiler')){
   console.error("\nnode app [environment] [server]");
-  console.error("environments: [development, production], servers: [proxy, tiler, old_tiler, carto_tiler, cached_carto_tiler]\n");
+  console.error("environments: [development, production], servers: [proxy, tiler]\n");
   process.exit(1);
 }
 
