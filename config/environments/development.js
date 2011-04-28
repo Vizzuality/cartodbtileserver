8f0123cce2efacc916bd68b1b46b8b7bc5c1c3c3
Simon Tokumine <simon@japancentre.com>
2011-04-28 15:34:29 +0100

    update environment settings for test deploy

--- config/environments/development.js
+++ config/environments/development.js
@@ -1,5 +1,7 @@
 module.exports.environment  = 'development';
-module.exports.db_base_name = 'cartodb_dev_user_{user_id}_db';
+module.exports.db_base_name = 'cartodb_dev_user_<%= user_id %>_db';
 module.exports.db_user      = 'publicuser';
 module.exports.db_host      = '127.0.0.1';
+module.exports.redis_host   = '127.0.0.1';
+module.exports.redis_port   = 6379;
 module.exports.carto_port   = 8080;
--- config/environments/production.js
+++ config/environments/production.js
@@ -1,5 +1,7 @@
 module.exports.environment  = 'production';
-module.exports.db_base_name = 'cartodb_user_{user_id}_db';
+module.exports.db_base_name = 'cartodb_user_<%= user_id %>_db';
 module.exports.db_user      = 'postgres';
 module.exports.db_host      = '10.211.14.63';
+module.exports.redis_host   = '10.211.14.63';
+module.exports.redis_port   = 6379;
 module.exports.carto_port   = 8080;
