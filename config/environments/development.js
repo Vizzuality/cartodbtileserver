da910645e8c84e03c80760ba8c3ab2962d0dd94b
Simon Tokumine <simon@japancentre.com>
2011-05-31 15:58:58 +0100

    updated user'

--- config/environments/development.js
+++ config/environments/development.js
@@ -1,6 +1,6 @@
 module.exports.environment  = 'development';
 module.exports.db_base_name = 'cartodb_dev_user_<%= user_id %>_db';
-module.exports.db_user      = 'publicuser';
+module.exports.db_user      = 'postgres';
 module.exports.db_host      = 'localhost';
 module.exports.redis_host   = '127.0.0.1';
 module.exports.redis_port   = 6379;
